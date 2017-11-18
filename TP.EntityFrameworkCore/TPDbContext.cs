53599d18eb62467f1ec718a6cb0810e589d1066f
孤鸿 夜 <litiansum@foxmail.com>
2017-11-18 14:38:48 +0800

    添加EF框架

---
+++ TP.EntityFrameworkCore/TPDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP.EntityFrameworkCore
+{
+    public class TPDbContext : DbContext
+    {
+        public TPDbContext() : base("Default")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ TP.Mvc.Web/Controllers/UploaderController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace TP.Mvc.Web.Controllers
+{
+    public class UploaderController : Controller
+    {
+
+        public JsonResult UploadImage(HttpPostedFileBase uploadedFile)
+        {
+            if (uploadedFile != null && uploadedFile.ContentLength > 0)
+            {
+                string filePath = Server.MapPath("/App_Data/Upload/Images");
+                if (!Directory.Exists(filePath))
+                {
+                    Directory.CreateDirectory(filePath);
+                }
+                string fileName = Path.Combine(filePath, uploadedFile.FileName);
+                if (System.IO.File.Exists(fileName))
+                    System.IO.File.Delete(fileName);
+                uploadedFile.SaveAs(fileName);
+                return Json(new
+                {
+                    success = true,
+                    message = "上传成功"
+                }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new
+                {
+                    success = false,
+                    message = "无效的文件"
+                }, JsonRequestBehavior.AllowGet);
+            }
+           
+        }
+    }
+}
