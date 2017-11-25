44ab0f4af3624eef1f1fbcf55c159e1c95c2c2c6
孤鸿 夜 <litiansum@foxmail.com>
2017-11-25 23:41:26 +0800

    添加日志过滤方法

--- TP.Mvc.Web/Global.asax.cs
+++ TP.Mvc.Web/Global.asax.cs
@@ -17,5 +17,27 @@ namespace TP.Mvc.Web
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        //添加Elmah日志记录过滤器
+        void ErrorLog_Filtering(object sender,Elmah.ExceptionFilterEventArgs e)
+        {
+            var exception = e.Exception.GetBaseException();
+            if (exception is HttpRequestValidationException)
+            {
+                e.Dismiss();
+            }
+        }
+        void ErrorEmail_Filtering(object sender, Elmah.ExceptionFilterEventArgs e)
+        {
+            var exception = e.Exception.GetBaseException();
+            var httpException = (HttpException)e.Exception;
+            if (httpException != null && httpException.GetHttpCode() == 404)
+            {
+                e.Dismiss();
+            }
+            if (exception is System.IO.FileNotFoundException || exception is HttpRequestValidationException || exception is httpException)
+            {
+                e.Dismiss();
+            }
+        }
     }
 }
